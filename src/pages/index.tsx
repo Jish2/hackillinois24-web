f40949fae0f1c3b7a8f8fc9a72f2619063683145
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-09 13:19:03 -0500

    full index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,87 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { FetchError } from "@/components/fetch-error";
+import { EventCard } from "@/components/event-card";
+import { CalendarPicker } from "@/components/calendar-picker";
+import { Guide } from "@/components/guide";
 
-const inter = Inter({ subsets: ['latin'] })
+import { EVENTS_API } from "@/constants/api-url";
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
+import type { Event } from "@/types/event.type";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+async function getEvents(): Promise<Event[] | null> {
+	try {
+		const url = EVENTS_API;
+		const response = await fetch(url);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		if (!response?.ok) {
+			return null;
+		}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		const { events } = await response.json();
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+		return events;
+	} catch (error) {
+		return null;
+		/*
+		 * In getStaticProps, if props === null, then the last
+		 * cached data will be served.
+		 */
+	}
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+/*
+ * Because I was unsure which Data Fetching method was "optimal" as
+ * this is dependent on how frequent the data will update,
+ *
+ * I have both implementations for getServerSideProps and
+ * getStaticProps.
+ */
+
+/*
+export const getServerSideProps: GetServerSideProps<{
+	events: Event[] | null;
+}> = async () => {
+	const events = await getEvents();
+	return { props: { events }, revalidate: Number(process.env.REVALIDATE) || 60 };
+};
+ */ // InferGetServerSidePropsType<typeof getServerSideProps>
+
+export const getStaticProps: GetStaticProps<{
+	events: Event[] | null;
+}> = async () => {
+	const events = await getEvents();
+	return { props: { events }, revalidate: Number(process.env.REVALIDATE) || 60 };
+};
+
+export default function Home({ events }: InferGetStaticPropsType<typeof getStaticProps>) {
+	return (
+		<div id="main-wrapper" className="flex sm:flex-row flex-col sm:overflow-clip">
+			<aside
+				id="sidebar-wrapper"
+				className="w-full sm:max-w-[240px] sm:h-screen-nav flex sm:flex-col p-8 gap-12 justify-between sm:justify-normal max-w-[400px] mx-auto"
+			>
+				<CalendarPicker events={events} />
+				<Guide />
+			</aside>
+			<main id="main-content" className="pt-0 sm:pt-8 flex-1 sm:h-screen-nav sm:overflow-y-scroll p-8 flex flex-col gap-4 sm:pl-0">
+				{events === null ? (
+					/*
+					 * this error component will only display if getServerSideProps is used,
+					 * arbitrary for this implementation, as errors in fetch will prevent build from completing
+					 */
+					<FetchError />
+				) : (
+					<>
+						<div id="day-label-container" className="sm:flex sm:items-end sm:gap-4">
+							<h1 className="text-3xl font-semibold">Friday, April 9</h1>
+							<span className="text-gray-400 font-light sm:text-lg">CDT TIME</span>
+						</div>
+						{events.map((event, index) => {
+							return <EventCard event={event} key={event.id} index={index} />;
+						})}
+					</>
+				)}
+			</main>
+		</div>
+	);
 }
