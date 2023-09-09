d0000b15ffb93e50b5296400137a9e422a216d58
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-09 06:47:12 -0500

    add navbar to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { Providers } from "@/pages/providers";
+import { CustomNavbar } from "@/components/navbar";
 import { Inter } from "next/font/google";
+
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Providers>
+			<CustomNavbar />
 			<Component {...pageProps} className={inter.className} />
 		</Providers>
 	);
