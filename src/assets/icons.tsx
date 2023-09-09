ae26fc2910024aff9905e80b44e150882b4e62d4
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-09 02:09:06 -0500

    add cn, and location icon

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -4,9 +4,10 @@ interface Icon {
 	fill?: string;
 	stroke?: string;
 	strokeWidth?: string;
+	className?: string;
 }
 
-export const MegaphoneIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => (
+export const MegaphoneIcon = ({ width, height, fill, stroke, strokeWidth, className }: Icon) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width={width ?? "24"}
@@ -17,13 +18,14 @@ export const MegaphoneIcon = ({ width, height, fill, stroke, strokeWidth }: Icon
 		strokeWidth={strokeWidth ?? "2"}
 		strokeLinecap="round"
 		strokeLinejoin="round"
+		className={className}
 	>
 		<path d="m3 11 18-5v12L3 14v-3z" />
 		<path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
 	</svg>
 );
 
-export const SunIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => (
+export const SunIcon = ({ width, height, fill, stroke, strokeWidth, className }: Icon) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width={width ?? "24"}
@@ -34,6 +36,7 @@ export const SunIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => (
 		strokeWidth={strokeWidth ?? "2"}
 		strokeLinecap="round"
 		strokeLinejoin="round"
+		className={className}
 	>
 		<circle cx="12" cy="12" r="4" />
 		<path d="M12 2v2" />
@@ -47,7 +50,7 @@ export const SunIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => (
 	</svg>
 );
 
-export const MoonIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => (
+export const MoonIcon = ({ width, height, fill, stroke, strokeWidth, className }: Icon) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width={width ?? "24"}
@@ -58,7 +61,26 @@ export const MoonIcon = ({ width, height, fill, stroke, strokeWidth }: Icon) => 
 		strokeWidth={strokeWidth ?? "2"}
 		strokeLinecap="round"
 		strokeLinejoin="round"
+		className={className}
 	>
 		<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+	</svg>
+);
+
+export const LocationIcon = ({ width, height, fill, stroke, strokeWidth, className }: Icon) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={width ?? "24"}
+		height={height ?? "24"}
+		viewBox="0 0 24 24"
+		fill={fill ?? "none"}
+		stroke={stroke ?? "currentColor"}
+		strokeWidth={strokeWidth ?? "2"}
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={className}
+	>
+		<path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+		<circle cx="12" cy="10" r="3" />
 	</svg>
 );
