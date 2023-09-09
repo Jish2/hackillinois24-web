4ecb6fd914ca08661ac197ed1f790b3ab13cd4ef
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-09 06:45:15 -0500

    validate email + title util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,3 +23,15 @@ export function formatTime(input: number): string {
 
 	return time;
 }
+
+export function validateEmail(email: string) {
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+}
+
+export function titleString(str: string) {
+	return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+}
