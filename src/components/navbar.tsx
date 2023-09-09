f2f8f2aae9fc17af846f03836f950a40c9e1e0c0
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-09 06:45:31 -0500

    navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/navbar";
+import { HackIllinoisLogo } from "@/assets/icons";
+import Link from "next/link";
+
+import { NAVBAR_ITEMS } from "@/constants";
+import { NewsletterInput } from "./newsletter-input";
+
+export function CustomNavbar() {
+	return (
+		<Navbar isBordered>
+			<NavbarBrand>
+				<HackIllinoisLogo width={120} height={40} />
+				{/* <p className="font-bold text-inherit"></p> */}
+			</NavbarBrand>
+
+			<NavbarContent className="hidden sm:flex gap-8" justify="center">
+				{NAVBAR_ITEMS.map((item, index) => (
+					<NavbarItem key={index}>
+						<Link color="foreground" href={item.href}>
+							{item.label}
+						</Link>
+					</NavbarItem>
+				))}
+			</NavbarContent>
+
+			<NavbarContent as="div" justify="end">
+				<NewsletterInput />
+			</NavbarContent>
+		</Navbar>
+	);
+}
