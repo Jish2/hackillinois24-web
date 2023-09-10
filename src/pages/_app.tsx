e225e06c384962dda3c41e6a66e899ee9b59a581
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-10 00:36:31 -0500

    meta and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,16 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Providers>
+			<Head>
+				<title>HackIllinois Schedule</title>
+			</Head>
 			<CustomNavbar />
 			<Component {...pageProps} className={inter.className} />
 		</Providers>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en" className="dark">
-			<Head />
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="author" content="jgoon" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
